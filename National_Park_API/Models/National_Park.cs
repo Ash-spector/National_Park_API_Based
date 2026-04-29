ca18053b3bf70593d08d689e77236bf47f55dfee
Ash-spector <shuklaaniket090@gmail.com>
2026-04-29 23:33:16 +0530

    Add project files.

---
+++ National_Park_API/Models/National_Park.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace National_Park_API.Models
+{
+    public class National_Park
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string State { get; set; }
+        public byte[] ? Picture { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Established { get; set; }
+    }
+}
---
+++ National_Park_API/Models/Trail.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace National_Park_API.Models
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Distance { get; set; }
+        [Required]
+        public string Elevation { get; set; }
+        public DateTime DateCreated { get; set; }
+        public enum Difficulty { Easy, Moderate, Difficult }
+        public Difficulty typeDifficult { get; set; }
+        public int NationalParkId { get; set; }
+        [ForeignKey("NationalParkId")]
+        public National_Park NationalPark { get; set; }
+    }
+}
