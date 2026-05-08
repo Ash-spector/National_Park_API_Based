2d324b21b5500ff7a6cb8313d089a12b5c675284
Ash-spector <shuklaaniket090@gmail.com>
2026-05-08 23:35:53 +0530

    Add home controller

---
+++ NationalParkWebApp/Models/ViewModels/IndexVM.cs
@@ -0,0 +1,8 @@
+﻿namespace NationalParkWebApp.Models.ViewModels
+{
+    public class IndexVM
+    { 
+        public IEnumerable<NationalPark> NationalParkList { get; set; }
+        public IEnumerable<Trail> TrailList { get; set; }
+    }
+}
